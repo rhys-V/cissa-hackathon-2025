e3d9bf44492b61ea093470e0323c83fc291b2e5a
Rhys Vu <s4093409@student.rmit.edu.au>
2025-04-25 15:01:06 +1000

    making beginning of taskbar

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -3,6 +3,17 @@ import Counter from "../components/Counter";
 export default function HomePage() {
    return (
       <main>
+         <header>
+                    <div className="header-style">
+                        <nav>
+                            <ul>
+                                <li><a href="/">Home</a></li>
+                                <img src="path/to/image.jpg" alt="Website Logo"></img>
+                                <li><a href="/page2">Home</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </header>
          <h1 id="title">Scamify</h1>
          <Counter />
       </main>
